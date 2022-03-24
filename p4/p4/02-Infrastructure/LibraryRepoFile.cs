ee84eb0378ecd85703b61d02813de48b037320c3
Amirghk <ghkamirhossein@gmail.com>
2022-03-24 17:52:54 +0430

    working on file Repo

--- p4/p4/02-Infrastructure/LibraryRepoFile.cs
+++ p4/p4/02-Infrastructure/LibraryRepoFile.cs
@@ -1,28 +1,64 @@
-﻿public class LibraryRepoFile : ILibraryRepository
+﻿using Newtonsoft.Json;
+public class LibraryRepoFile : ILibraryRepository
 {
+    string booksAddress = @".\books.txt";
+    string membersAddress = @".\members.txt";
+    string librariansAddress = @".\librarians.txt";
+    string writersAddress = @".\writers.txt";
+    
+    //
+    
+
     public void AddBook(Book book)
     {
-        throw new NotImplementedException();
+        // deserialize txt file to a list
+        var books = JsonConvert.DeserializeObject<List<Book>>(File.ReadAllText(booksAddress));
+        books.Add(book); // add the item to the list
+
+        // serialize the list and write it into the file
+        string output = JsonConvert.SerializeObject(books);
+        File.WriteAllText(booksAddress, output);
     }
 
     public void AddBookWriter(BookWriter bookWriter)
     {
-        throw new NotImplementedException();
+        var writers = JsonConvert.DeserializeObject<List<BookWriter>>(File.ReadAllText(writersAddress));
+        writers.Add(bookWriter);
+
+        string output = JsonConvert.SerializeObject(writers);
+        File.WriteAllText(writersAddress, output);
     }
 
     public void AddLibrarian(Librarian librarian)
     {
-        throw new NotImplementedException();
+        var librarians = JsonConvert.DeserializeObject<List<Librarian>>(File.ReadAllText(librariansAddress));
+        librarians.Add(librarian);
+
+        string output = JsonConvert.SerializeObject(librarians);
+        File.WriteAllText(librariansAddress, output);
     }
 
     public void AddMember(Member member)
     {
-        throw new NotImplementedException();
+        var members = JsonConvert.DeserializeObject<List<Member>>(File.ReadAllText(membersAddress));
+        members.Add(member);
+
+        string output = JsonConvert.SerializeObject(members);
+        File.WriteAllText(membersAddress, output);
     }
 
     public List<Article> GetArticles()
     {
-        throw new NotImplementedException();
+        // deserialize txt file into list
+        var books = JsonConvert.DeserializeObject<List<Book>>(File.ReadAllText(booksAddress));
+        var articles = books.FindAll(b => b.Role == Roles.article); // find all the books that are articles
+        List<Article> articlesList = new(); // make a new article list
+
+        foreach (var article in articlesList) 
+        {
+            articles.Add((Article) article); // unbox books into articles and add to the list
+        }
+        return articlesList; // return list
     }
 
     public Book GetBook(string name)
--- p4/p4/03-EndPoint/Program.cs
+++ p4/p4/03-EndPoint/Program.cs
@@ -1,6 +1,11 @@
-﻿
-//StoryBook st = (StoryBook) DataStore.books[0];
-//st.GetInfo();
+﻿using Newtonsoft.Json;
+
+// json convert settings to ot be stuck in infinite loop
+JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+{
+    Formatting = Newtonsoft.Json.Formatting.Indented,
+    ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+};
 
 LibraryRepository lb = new();
 var storyBooks = lb.GetStoryBooks();
